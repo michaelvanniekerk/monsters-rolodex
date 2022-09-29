614ac8ccee125444325c4796d8ce586ee77f15b6
michaelvanniekerk <38032789+michaelvanniekerk@users.noreply.github.com>
2022-09-29 21:00:03 +0200

    Initial filtering of monsters based on name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      filter: "",
+    };
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    console.log("mount");
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+      response.json().then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      )
+    );
+  }
+
+  onFilterChange = (event) => {
+    this.setState(() => ({
+      filter: event.target.value.toLocaleLowerCase(),
+    }));
+  };
+
+  render() {
+    console.log("render");
+
+    const { monsters, filter } = this.state;
+    const { onFilterChange } = this;
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={onFilterChange}
+        />
+
+        {monsters
+          .filter((monster) =>
+            monster.name.toLocaleLowerCase().includes(filter)
+          )
+          .map((monster) => {
+            return <h1 key={monster.id}>{monster.name}</h1>;
+          })}
+      </div>
+    );
+  }
 }
 
 export default App;
